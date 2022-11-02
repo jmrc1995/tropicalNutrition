b140e545dc9144b6ca7c258b8b649cb7bf80a1d4
Dani Lucia <danilucia@Danis-MacBook.fios-router.home>
2022-11-02 13:21:09 -0400

    updated ui for sort method

--- Components/NutritionFilter.js
+++ Components/NutritionFilter.js
@@ -7,9 +7,10 @@ import {
   Heading,
   Select,
   Flex,
+  Text
 } from "@chakra-ui/react";
 import { agedCheese } from "../utils/Cheese";
-import { useState } from "react";
+import { imageConfigDefault } from "next/dist/shared/lib/image-config";
 
 function NutritionFilter() {
   let [sortType, setSortType] = React.useState(" ");
@@ -17,45 +18,47 @@ function NutritionFilter() {
 
   const sorted = (e) => {
     setSortType(e);
-    if (sortType === "asc") {
-      agedCheese.sort((a, b) => {
-        return a.cholesterol - b.cholesterol;
-      });
-    } else {
+    if (sortType === "dsc") {
       agedCheese.sort((a, b) => {
         return b.cholesterol - a.cholesterol;
       });
+    } else {
+      agedCheese.sort((a, b) => {
+        return a.cholesterol - b.cholesterol;
+      });
     }
+    setSortedArr(agedCheese);
   };
-  
-
-
-  console.log(sortedArr)
-
-  setSortedArr(agedCheese)
 
   return (
-
-    <Box>
-      <Grid templateRows="repeat(2, 1fr)" justifyItems={"center"}>
-        <GridItem>
-          <Heading>Aged Cheeses</Heading>
+    <Box w={"100%"}>
+      <Grid templateColumns="repeat(4, 1fr)"  >
+        <GridItem gridColumnStart={2} gri mt={10} w={"100%"} colSpan={2}>
+          <Heading mb={4}>Aged Cheeses</Heading>
           <Select
             placeholder="Filter by nutrition"
             onChange={(e) => sorted(e.target.value)}
-            // value={sortType}
           >
             <option value={"dsc"}>Cholesterol: Low to High</option>
             <option value={"asc"}>Cholesterol: High to Low</option>
           </Select>
-          <GridItem>
-            <Flex flexDirection={"column"}>
-              {sortedArr.map((item, index) => {
-                <Box key={index}>
-                  <Heading>{item.name}</Heading>
-             
-                </Box>;
-              })}
+          <GridItem w={"100%"}>
+            <Flex >
+              {agedCheese
+                ? agedCheese.map((item, index) => (
+                    <Box key={index} p={10}>
+                      <Heading my={4}>{item.name}</Heading>
+                      <Image src={item.image} boxSize={60} />
+                      <Text mt={2}>Cholesterol: {item.cholesterol}</Text>
+                    </Box>
+                  ))
+                : sortedArr.map((item, index) => (
+                    <Box key={index} p={10}>
+                      <Heading my={4}>{item.name}</Heading>
+                      <Image src={item.image} boxSize={60} />
+                      <Text>Cholesterol: {item.cholesterol}</Text>
+                    </Box>
+                  ))}
             </Flex>
           </GridItem>
         </GridItem>
